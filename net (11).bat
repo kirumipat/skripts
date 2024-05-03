66eddfe4610328a9053298afbdb8cf7cd67899c6
Kirumi <49292090+kirumipat@users.noreply.github.com>
2024-05-03 12:00:28 +0300

    Add files via upload

---
+++ net (11).bat
@@ -0,0 +1,181 @@
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh int ip reset
+netsh int ipv4 reset
+netsh int ipv6 reset
+netsh int tcp reset
+netsh winsock reset
+netsh branchcache reset
+netsh http flush logbuffer
+netsh int tcp set global autotuninglevel=disabled
+netsh int tcp set global ecncapability=disabled
+netsh int tcp set global dca=enabled
+netsh int tcp set global netdma=enabled
+netsh int tcp set global rsc=enabled
+netsh int tcp set global rss=enabled
+netsh int tcp set global timestamps=disabled
+netsh int tcp set global initialRto=2000
+netsh interface ipv4 set subinterface “Ethernet” mtu=1480 store=persistent
+netsh int tcp set global nonsackrttresiliency=disabled
+netsh int tcp set global maxsynretransmissions=2
+netsh int tcp set security mpp=disabled
+netsh int tcp set security profiles=disabled
+netsh int tcp set heuristics disabled
+netsh int ip set global neighborcachelimit=4096
+netsh int tcp set supplemental Internet congestionprovider=CUBIC
+netsh int ip set global taskoffload=disabled
+netsh int ipv6 set state disabled
+netsh int isatap set state disabled
+netsh int teredo set state disabled
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d "4294967295" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Psched" /v "NonBestEffortLimit" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\QoS" /v "Do not use NLA" /t REG_SZ /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Ndis\Parameters" /v "RssBaseCpu" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DefaultTTL" /t REG_DWORD /d "64" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "Tcp1323Opts" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpMaxDupAcks" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "SackOpts" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "MaxUserPort" /t REG_DWORD /d "65534" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpTimedWaitDelay" /t REG_DWORD /d "30" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "LocalPriority" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "HostsPriority" /t REG_DWORD /d "5" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "DnsPriority" /t REG_DWORD /d "6" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "NetbtPriority" /t REG_DWORD /d "7" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Winsock" /v "MinSockAddrLength" /t REG_DWORD /d "16" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Winsock" /v "MaxSockAddrLength" /t REG_DWORD /d "16" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /v "TcpAckFrequency" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /v "TCPNoDelay" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /v "TcpDelAckTicks" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v "DODownloadMode" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v "DownloadMode" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Settings" /v "DownloadMode" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "autodisconnect" /t REG_DWORD /d "4294967295" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "Size" /t REG_DWORD /d "3" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "EnableOplocks" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "IRPStackSize" /t REG_DWORD /d "20" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "SharingViolationDelay" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "SharingViolationRetries" /t REG_DWORD /d "0" /f
+for /f %%n in ('Reg query "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}" /v "*SpeedDuplex" /s ^| findstr  "HKEY"') do (
+reg add "%%n" /v "AutoPowerSaveModeEnabled" /t REG_SZ /d "0" /f
+reg add "%%n" /v "AutoDisableGigabit" /t REG_SZ /d "0" /f
+reg add "%%n" /v "AdvancedEEE" /t REG_SZ /d "0" /f
+reg add "%%n" /v "DisableDelayedPowerUp" /t REG_SZ /d "2" /f
+reg add "%%n" /v "NicAutoPowerSaver" /t REG_SZ /d "2" /f
+reg add "%%n" /v "PowerDownPll" /t REG_SZ /d "0" /f
+reg add "%%n" /v "PowerSavingMode" /t REG_SZ /d "0" /f
+reg add "%%n" /v "ReduceSpeedOnPowerDown" /t REG_SZ /d "0" /f
+reg add "%%n" /v "SmartPowerDownEnable" /t REG_SZ /d "0" /f
+reg add "%%n" /v "S5NicKeepOverrideMacAddrV2" /t REG_SZ /d "0" /f
+reg add "%%n" /v "S5WakeOnLan" /t REG_SZ /d "0" /f
+reg add "%%n" /v "ULPMode" /t REG_SZ /d "0" /f
+reg add "%%n" /v "WakeOnDisconnect" /t REG_SZ /d "0" /f
+reg add "%%n" /v "WakeOnLink" /t REG_SZ /d "0" /f
+reg add "%%n" /v "WolShutdownLinkSpeed" /t REG_SZ /d "2" /f
+reg add "%%n" /v "JumboPacket" /t REG_SZ /d "1514" /f
+reg add "%%n" /v "TransmitBuffers" /t REG_SZ /d "4096" /f
+reg add "%%n" /v "ReceiveBuffers" /t REG_SZ /d "512" /f
+reg add "%%n" /v "RSS" /t REG_SZ /d "1" /f
+reg add "%%n" /v "RSSProfile" /t REG_SZ /d "3" /f
+reg add "%%n" /v "*NumRssQueues" /t REG_SZ /d "2" /f
+reg add "%%n" /v "*FlowControl" /t REG_SZ /d "0" /f
+reg add "%%n" /v "FlowControlCap" /t REG_SZ /d "0" /f
+reg add "%%n" /v "TxIntDelay" /t REG_SZ /d "0" /f
+reg add "%%n" /v "TxAbsIntDelay" /t REG_SZ /d "0" /f
+reg add "%%n" /v "RxIntDelay" /t REG_SZ /d "0" /f
+reg add "%%n" /v "RxAbsIntDelay" /t REG_SZ /d "0" /f
+reg add "%%n" /v "FatChannelIntolerant" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*InterruptModeration" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*EEE" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnablePME" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EEELinkAdvertisement" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableGreenEthernet" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableSavePowerNow" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnablePowerManagement" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableDynamicPowerGating" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableConnectedPowerGating" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableWakeOnLan" /t REG_SZ /d "0" /f
+reg add "%%n" /v "GigaLite" /t REG_SZ /d "0" /f
+reg add "%%n" /v "IPChecksumOffloadIPv4" /t REG_SZ /d "0" /f
+reg add "%%n" /v "LsoV1IPv4" /t REG_SZ /d "0" /f
+reg add "%%n" /v "LsoV2IPv4" /t REG_SZ /d "0" /f
+reg add "%%n" /v "LsoV2IPv6" /t REG_SZ /d "0" /f
+reg add "%%n" /v "PMARPOffload" /t REG_SZ /d "0" /f
+reg add "%%n" /v "PMNSOffload" /t REG_SZ /d "0" /f
+reg add "%%n" /v "TCPChecksumOffloadIPv4" /t REG_SZ /d "0" /f
+reg add "%%n" /v "TCPChecksumOffloadIPv6" /t REG_SZ /d "0" /f
+reg add "%%n" /v "UDPChecksumOffloadIPv6" /t REG_SZ /d "0" /f
+reg add "%%n" /v "UDPChecksumOffloadIPv4" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*WakeOnMagicPacket" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*WakeOnPattern" /t REG_SZ /d "0" /f
+reg add "%%n" /v "WakeOnLink" /t REG_SZ /d "0" /f
+)
+powershell "Get-NetAdapter -IncludeHidden | Set-NetIPInterface -WeakHostSend Enabled -WeakHostReceive Enabled -ErrorAction SilentlyContinue"
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Application Name" /t REG_SZ /d "fortniteclient-win64-shipping.exe" /f 
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "DSCP value" /t REG_SZ /d "46" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Local IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Local IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Local Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Protocol" /t REG_SZ /d "UDP" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Remote IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Remote IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "Remote Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "throttle Rate" /t REG_SZ /d "-1" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\fortnite" /v "version" /t REG_SZ /d "1.0" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Application Name" /t REG_SZ /d "VALORANT-Win64-Shipping.exe" /f 
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "DSCP value" /t REG_SZ /d "46" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Local IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Local IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Local Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Protocol" /t REG_SZ /d "UDP" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Remote IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Remote IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "Remote Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "throttle Rate" /t REG_SZ /d "-1" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\valorant" /v "version" /t REG_SZ /d "1.0" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Application Name" /t REG_SZ /d "dota2.exe" /f 
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "DSCP value" /t REG_SZ /d "46" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Local IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Local IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Local Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Protocol" /t REG_SZ /d "UDP" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Remote IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Remote IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "Remote Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "throttle Rate" /t REG_SZ /d "-1" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\dota2" /v "version" /t REG_SZ /d "1.0" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Application Name" /t REG_SZ /d "cs2.exe" /f 
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "DSCP value" /t REG_SZ /d "46" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Local IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Local IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Local Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Protocol" /t REG_SZ /d "UDP" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Remote IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Remote IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Remote Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "throttle Rate" /t REG_SZ /d "-1" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "version" /t REG_SZ /d "1.0" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Application Name" /t REG_SZ /d "cs2.exe" /f 
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "DSCP value" /t REG_SZ /d "46" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Local IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Local IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Local Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Protocol" /t REG_SZ /d "UDP" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Remote IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Remote IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "Remote Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "throttle Rate" /t REG_SZ /d "-1" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\cs2" /v "version" /t REG_SZ /d "1.0" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Application Name" /t REG_SZ /d "r5apex.exe" /f 
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "DSCP value" /t REG_SZ /d "46" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Local IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Local IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Local Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Protocol" /t REG_SZ /d "UDP" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Remote IP" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Remote IP Prefix Length" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "Remote Port" /t REG_SZ /d "*" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "throttle Rate" /t REG_SZ /d "-1" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOftWARE\Policies\Microsoft\Windows\QoS\apex" /v "version" /t REG_SZ /d "1.0" /f
+gpupdate /force
+
